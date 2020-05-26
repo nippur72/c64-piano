f50e7bff57f71365b51800278abda0273e46082a
francesco.sblendorio <francesco.sblendorio@eidosmedia.com>
2020-05-26 20:23:21 +0200

    config_siel

--- m.bat
+++ m.bat
@@ -2,7 +2,7 @@
 rem node freq.js > freq.lm
 set name=piano
 rem call asmproc -i %name%.lm -o %name%.asm -d KBD_SIEL
-call asmproc -i %name%.lm -o %name%.asm
+call asmproc -i %name%.lm -o %name%.eq
 rem call asmproc -i %name%.lm -o %name%.asm -d DEBUG
 dasm %name%.asm -o%name%.prg -l%name%.lst
 
