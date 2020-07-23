8e89d815348718fec7ccd8cd4e71a750bbeaa0d4
nino-porcino <nino.porcino@gmail.com>
2020-07-23 17:45:04 +0200

    compile for dasm and ca65

--- m.bat
+++ m.bat
@@ -1,9 +1,16 @@
 @echo off
 rem node freq.js > freq.lm
+
 set name=piano
-rem call asmproc -i %name%.lm -o %name%.asm -d KBD_SIEL
-call asmproc -i %name%.lm -o %name%.asm
-rem call asmproc -i %name%.lm -o %name%.asm -d DEBUG
-dasm %name%.asm -o%name%.prg -l%name%.lst
+set compiler=ca65
+
+rem compila per tastiera siel
+rem set optsiel=-d KBD_SIEL
+
+if %compiler%==dasm call asmproc -i %name%.lm -o %name%.asm --target dasm -d TARGET_DASM %optsiel%
+if %compiler%==dasm dasm %name%.asm -o%name%.prg -l%name%.lst
+
+if %compiler%==ca65 call asmproc -i %name%.lm -o %name%.asm --target ca65 -d TARGET_CA65 %optsiel%
+if %compiler%==ca65 cl65 --config c64-asm.cfg %name%.asm -o %name%.prg
 
 
