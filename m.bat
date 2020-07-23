169db6fda51ddaae8670bd3c5dd6733c5fbcedff
nino-porcino <nino.porcino@gmail.com>
2020-07-23 17:49:24 +0200

    add emulated keyboard option

--- m.bat
+++ m.bat
@@ -4,13 +4,14 @@ rem node freq.js > freq.lm
 set name=piano
 set compiler=ca65
 
-rem compila per tastiera siel
-rem set optsiel=-d KBD_SIEL
+rem compila per tastiera siel o per tastiera emulata
+rem set optsiel=KBD_SIEL
+set optsiel=KBD_EMULATED
 
-if %compiler%==dasm call asmproc -i %name%.lm -o %name%.asm --target dasm -d TARGET_DASM %optsiel%
+if %compiler%==dasm call asmproc -i %name%.lm -o %name%.asm --target dasm -d %optsiel%,TARGET_DASM
 if %compiler%==dasm dasm %name%.asm -o%name%.prg -l%name%.lst
 
-if %compiler%==ca65 call asmproc -i %name%.lm -o %name%.asm --target ca65 -d TARGET_CA65 %optsiel%
+if %compiler%==ca65 call asmproc -i %name%.lm -o %name%.asm --target ca65 -d %optsiel%,TARGET_CA65
 if %compiler%==ca65 cl65 --config c64-asm.cfg %name%.asm -o %name%.prg
 
 
